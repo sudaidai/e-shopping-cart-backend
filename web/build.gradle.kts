309f744e957a0dd34122948ac0a3f725ed688deb
sudaidai <57120082+sudaidai@users.noreply.github.com>
2024-03-10 19:40:47 +0800

    Update build.gradle.kts

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("org.springframework.boot") version "3.2.3"
 	id("io.spring.dependency-management") version "1.1.4"
 	id("org.hibernate.orm") version "6.4.4.Final"
+	id("org.graalvm.buildtools.native") version "0.9.28"
 	kotlin("jvm") version "1.9.22"
 	kotlin("plugin.spring") version "1.9.22"
 	kotlin("plugin.jpa") version "1.9.22"
