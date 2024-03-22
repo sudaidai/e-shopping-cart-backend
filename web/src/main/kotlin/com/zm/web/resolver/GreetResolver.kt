fb2484c1a0ee257b5776444e7cf58bd1e17b6f2d
sudaidai <57120082+sudaidai@users.noreply.github.com>
2024-03-22 23:02:54 +0800

    Greeting GraphQL

---
+++ web/src/main/kotlin/com/zm/web/resolver/GreetResolver.kt
@@ -0,0 +1,13 @@
+package com.zm.web.resolver
+
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.stereotype.Controller
+
+@Controller
+class GreetResolver {
+
+    @QueryMapping
+    fun hello(): String {
+        return "Hello, World!"
+    }
+}
